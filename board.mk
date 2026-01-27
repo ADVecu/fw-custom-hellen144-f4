90b97bbf03497613814aa05915351ae388ee4ac7
Daniel Gaviria Arboleda <daniel.gaviria.utp@gmail.com>
2026-01-27 18:39:52 -0500

    Update board.mk

--- board.mk
+++ board.mk
@@ -6,8 +6,15 @@ BOARDINC += $(BOARD_DIR)/generated/controllers/generated
 # defines SHORT_BOARD_NAME
 include $(BOARD_DIR)/meta-info.env
 
+DDEFS += -DEFI_SOFTWARE_KNOCK=FALSE
+DDEFS += -DDIAG_5VP_PIN=Gpio::MM100_SPI3_MOSI
+DDEFS += -DEFI_ELECTRONIC_THROTTLE_BODY=FALSE
+DDEFS += -DEFI_TCU=FALSE
+DDEFS += -DEEFI_ONBOARD_MEMS=TRUE
+DDEFS += -DEEFI_STORAGE_MFS=TRUE
+
 # this is usually used by Bluetooth
 DDEFS += $(PRIMARY_COMMUNICATION_PORT_USART2)
 
 # this includes knock and mux control settings
-include $(BOARDS_DIR)/hellen/hellen-common-mega144.mk
+include $(BOARDS_DIR)/hellen/hellen-common-mega100.mk
